b40c2d53b5909adf9e0e2a7486c989c83cfc2885
dauqu <sujeetsingh734321@gmail.com>
2022-12-29 18:51:40 +0530

    dfhdfh

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const api = "https://revival.dauqu.host/api";
-// export const api = "http://localhost:4000/api";
+// export const api = "https://revival.dauqu.host/api";
+export const api = "http://localhost:4000/api";
